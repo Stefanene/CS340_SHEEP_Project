d1b130d17dd495efdc456aa9cc3f2276e51e1421
Stefanene <stefanene@hotmail.com>
2023-03-02 18:04:21 -0800

    general select queries for all entities and working pages

--- database_website/app.js
+++ database_website/app.js
@@ -12,18 +12,80 @@ var exphbs = require('express-handlebars');     // Import express-handlebars
 app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
+
 /*
     ROUTES
 */
-app.get('/', function(req, res)                 // This is the basic syntax for what is called a 'route'
-    {
-        let query1 = "SELECT * FROM Sneakers;";               // Define our query
+app.get('/', function(req, res) {
+        
+    //Sneaker / index page
+    let query1 = "SELECT * FROM Sneakers;";               // Define our query
 
-        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+    db.pool.query(query1, function(error, rows, fields){    // Execute the query
+        res.render('index', {data: rows});                  // Render the index.hbs file, and also send the renderer
+    });   
+});
 
-            res.render('index', {data: rows});                  // Render the index.hbs file, and also send the renderer
-        })   
-    });                                         // requesting the web site.
+
+app.get('/locations', function(req, res) {
+
+    //StoreLocations page
+        
+    let queryL1 = "SELECT * FROM StoreLocations;";
+    db.pool.query(queryL1, function(errorL, rowsL, fieldsL){    // Execute the query
+        res.render('locations', {data: rowsL});                  // Render the index.hbs file, and also send the renderer
+    });   
+
+});
+
+
+app.get('/customers', function(req, res) {
+
+    //Customers page
+        
+    let queryL1 = "SELECT * FROM Customers;";
+    db.pool.query(queryL1, function(errorC, rowsC, fieldsC){    // Execute the query
+        res.render('customers', {data: rowsC});                  // Render the index.hbs file, and also send the renderer
+    });   
+
+});
+
+
+app.get('/employees', function(req, res) {
+
+    //Employees page
+        
+    let queryL1 = "SELECT * FROM Employees;";
+    db.pool.query(queryL1, function(errorE, rowsE, fieldsE){    // Execute the query
+        res.render('employees', {data: rowsE});                  // Render the index.hbs file, and also send the renderer
+    });   
+
+});
+
+
+app.get('/shoefinder', function(req, res) {
+
+    //SneakerToLocations page
+        
+    let queryL1 = "SELECT * FROM SneakerToLocations;";
+    db.pool.query(queryL1, function(errorSL, rowsSL, fieldsSL){    // Execute the query
+        res.render('shoefinder', {data: rowsSL});                  // Render the index.hbs file, and also send the renderer
+    });   
+
+});
+
+
+app.get('/sales', function(req, res) {
+
+    //Sales page
+        
+    let queryL1 = "SELECT * FROM Sales;";
+    db.pool.query(queryL1, function(errorS, rowsS, fieldsS){    // Execute the query
+        res.render('sales', {data: rowsS});                  // Render the index.hbs file, and also send the renderer
+    });   
+
+});
+
 
 /*
     LISTENER
